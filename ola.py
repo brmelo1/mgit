4d0b65c835eb3903c279d0b52111388a1afde21c
brmelo1 <99694863+brmelo1@users.noreply.github.com>
2023-05-12 09:58:17 -0400

    Update ola.py
    eita

--- ola.py
+++ ola.py
@@ -2,9 +2,9 @@ num=int(input(f'manda um nuemro: '))
 
 num1=int(input(f'manda um nuemro: '))
 
-if num>0 and num1<0:
+if num<5 and num1<5:
     print(f'valor e top')
-elif num<0 and num1>0:
+elif num>5 and num1>5:
     print(f'valor nao e')
 else:
     print(f'deu ruim olaaa')
