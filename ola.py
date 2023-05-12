6e4f61fbbc45ce0b8fc81f73d903f401f53b22c9
brmelo1 <brunnomengo@hotmail.com>
2023-05-12 09:55:19 -0400

    novo

---
+++ ola.py
@@ -0,0 +1,10 @@
+num=int(input(f'manda um nuemro: '))
+
+num1=int(input(f'manda um nuemro: '))
+
+if num>0 and num1<0:
+    print(f'valor e top')
+elif num<0 and num1>0:
+    print(f'valor nao e')
+else:
+    print(f'deu ruim olaaa')
