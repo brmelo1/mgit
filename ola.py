ebf8eecd10288e68ffc635057403f4a5d48fa22f
brmelo1 <brunnomengo@hotmail.com>
2023-06-02 23:41:24 -0400

    ola att

--- ola.py
+++ ola.py
@@ -2,7 +2,7 @@ num=int(input(f'manda um nuemro: '))
 
 num1=int(input(f'manda um nuemro: '))
 
-if num<5 and num1<5:
+if num<50 and num1<500:
     print(f'valor e top')
 elif num>5 and num1>5:
     print(f'valor nao e')
